24846f0192acf03c7d938d8d4daa9b738cbdfe04
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2024-05-08 13:45:10 +0000

    Make the file executable

---
+++ get_summary.py
@@ -0,0 +1,47 @@
+'''
+Module: get_summary
+This module reads the data from the excel file and writes the
+data to a new excel file.
+'''
+
+from sys import argv
+import pandas as pd
+
+
+def generate_fuel_summary():
+    '''
+    Function that generates a summary of the fuel details from the input
+    '''
+    # read the excel file
+    data_file = pd.read_excel(argv[1])
+
+    # sort the data by registration_num and ticket
+    data_file.sort_values(by=['Registration_num', 'Ticket'], inplace=True)
+
+    # Replace comma with decimal point in Quantity column
+    # data_file['Quantity'] = data_file['Quantity'].str.replace(
+    #     ',', '.').astype(float)
+    data_file['Quantity'] = data_file['Quantity'].str.replace(
+        ',', '.').str.replace('\xa0', '').astype(float)
+
+    # Select only the desired columns
+    selected_cols = ['Registration_num', 'Ticket',
+                     'Product_or_Article', 'Quantity', 'Amount_incl_Tax']
+    data_selected = data_file[selected_cols]
+
+    # Group data by Registration_num and Product_or_Article
+    grouped_data = data_file.groupby(['Registration_num', 'Product_or_Article']).agg({
+        'Quantity': 'sum',
+        'Amount_incl_Tax': 'sum'
+    }).reset_index()
+
+    # Write to separate sheets in the same Excel workbook
+    with pd.ExcelWriter('combined_results.xlsx') as writer:
+        data_selected.to_excel(writer, sheet_name='Summary', index=False)
+        grouped_data.to_excel(writer, sheet_name='Totals', index=False)
+
+    print("Data saved to 'combined_results.xlsx'")
+
+
+if __name__ == "__main__":
+    generate_fuel_summary()
