13df4bc0b5839372be313e38e4b22a9f513de810
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2025-01-06 09:33:23 +0000

    Improved the code to correctly select the columns of the file

--- get_summary.py
+++ get_summary.py
@@ -4,6 +4,7 @@ This module reads the data from the excel file and writes the
 data to a new excel file.
 '''
 
+import os
 from sys import argv
 import pandas as pd
 
@@ -14,9 +15,13 @@ def generate_fuel_summary():
     '''
     # read the excel file
     data_file = pd.read_excel(argv[1])
+    # Extract base name and format for output
+    formatted_name = os.path.splitext(os.path.basename(data_file))[0]
+    output_file = f"{formatted_name}_combined_results.xlsx"
+
 
     # sort the data by registration_num and ticket
-    data_file.sort_values(by=['Reg_num', 'Ticket'], inplace=True)
+    data_file.sort_values(by=['Registration_num', 'Ticket'], inplace=True)
 
     # Replace comma with decimal point in Quantity column
     # data_file['Quantity'] = data_file['Quantity'].str.replace(
@@ -25,23 +30,22 @@ def generate_fuel_summary():
         ',', '.').str.replace('\xa0', '').astype(float)
 
     # Select only the desired columns
-    selected_cols = ['Reg_num', 'Ticket',
+    selected_cols = ['Registration_num', 'Ticket',
                      'Product_or_Article', 'Quantity', 'Amount_incl_Tax']
     data_selected = data_file[selected_cols]
 
     # Group data by Registration_num and Product_or_Article
-    grouped_data = data_file.groupby(['Reg_num', 'Product_or_Article']).agg({
+    grouped_data = data_file.groupby(['Registration_num', 'Product_or_Article']).agg({
         'Quantity': 'sum',
         'Amount_incl_Tax': 'sum'
     }).reset_index()
 
     # Write to separate sheets in the same Excel workbook
-    with pd.ExcelWriter('combined_results.xlsx') as writer:
+    with pd.ExcelWriter(output_file) as writer:
         data_selected.to_excel(writer, sheet_name='Summary', index=False)
         grouped_data.to_excel(writer, sheet_name='Totals', index=False)
 
-    print("Data saved to 'combined_results.xlsx'")
-
+    print(f"Data saved to {formatted_name}")
 
 if __name__ == "__main__":
     generate_fuel_summary()
